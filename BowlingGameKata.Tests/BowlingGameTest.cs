cd954af5cba86c2e38686498f754378dd1fc8a5e
stefanov2081 <stefanov2081@gmail.com>
2020-11-25 15:06:38 +0200

    Test one spare
    Spare is whenever the player knocks all the pins with two rolls in one
    frame. In this case the player gets a bonus equal to the pins knocked by
    the first roll in the next frame.

--- BowlingGameKata.Tests/BowlingGameTest.cs
+++ BowlingGameKata.Tests/BowlingGameTest.cs
@@ -30,12 +30,28 @@ namespace BowlingGameKata.Tests
             Assert.AreEqual(20, game.Score());
         }
 
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            game.Roll(3);
+            RollMany(17, 0);
+
+            Assert.AreEqual(16, game.Score());
+        }
+
         private void RollMany(int n, int pins)
         {
             for (int i = 0; i < n; i++)
             {
                 game.Roll(pins);
             }
+        }
+
+        private void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
         }
     }
 }
--- BowlingGameKata/Game.cs
+++ BowlingGameKata/Game.cs
@@ -2,16 +2,39 @@
 {
     public class Game
     {
-        private int score = 0;
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
 
         public void Roll(int pins)
         {
-            score += pins;
+            rolls[currentRoll++] = pins;
         }
 
         public int Score()
         {
+            int score = 0;
+            int frameIndex = 0;
+            
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+
             return score;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
         }
     }
 }
